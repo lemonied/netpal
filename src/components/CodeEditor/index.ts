97bb4dcfbb0b465d0ce286ca67fe2d94d09a0a42
ChenJiYuan <447334358@qq.com>
2025-03-03 10:06:04 +0000

    development

--- src/components/CodeEditor/index.ts
+++ src/components/CodeEditor/index.ts
@@ -1,15 +1,31 @@
 import Editor, { loader } from '@monaco-editor/react';
 
-loader.config({
-  'vs/nls': {
-    availableLanguages: {
-      '*': 'zh-cn',
-    },
+import * as monaco from 'monaco-editor';
+import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker?worker';
+import jsonWorker from 'monaco-editor/esm/vs/language/json/json.worker?worker';
+import cssWorker from 'monaco-editor/esm/vs/language/css/css.worker?worker';
+import htmlWorker from 'monaco-editor/esm/vs/language/html/html.worker?worker';
+import tsWorker from 'monaco-editor/esm/vs/language/typescript/ts.worker?worker';
+
+self.MonacoEnvironment = {
+  getWorker(_, label) {
+    if (label === 'json') {
+      return new jsonWorker();
+    }
+    if (label === 'css' || label === 'scss' || label === 'less') {
+      return new cssWorker();
+    }
+    if (label === 'html' || label === 'handlebars' || label === 'razor') {
+      return new htmlWorker();
+    }
+    if (label === 'typescript' || label === 'javascript') {
+      return new tsWorker();
+    }
+    return new editorWorker();
   },
-  paths: {
-    vs: 'https://gw.alipayobjects.com/os/lib/monaco-editor/0.50.0/min/vs',
-  },
-});
+};
+
+loader.config({ monaco });
 
 export default Editor;
 
