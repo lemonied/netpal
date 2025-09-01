3f8ac5a8b98216f0645f674f791b7ce58fb974c9
ChenJiYuan <447334358@qq.com>
2025-09-01 10:50:32 +0000

    feat: development

--- eslint.config.js
+++ eslint.config.js
@@ -32,8 +32,6 @@ export default tseslint.config(
       'object-curly-spacing': ['error', 'always'],
       'no-console': 'warn',
       'quotes': ['error', 'single'],
-      'indent': ['error', 2, { SwitchCase: 1 }],
-      '@/indent': ['error', 2, { SwitchCase: 1 }],
       'keyword-spacing': 'error',
       'arrow-spacing': ['error', { before: true, after: true }],
       'camelcase': [
